b5eab9d3336c7711210ce39eeab548a6c19c1048
njoyaa <nemathulah.joyaa@tekscchool.us>
2022-08-09 12:31:56 -0700

    new project is being created for this sprint.

---
+++ src/test/java/TestRunner.java
@@ -0,0 +1,11 @@
+
+public class TestRunner {
+
+	@Karate.Test
+	public Karate runTest() {
+		// classpath is the location to your feature files.
+		// if this path is wrong you won't be able to run anything.
+		// tag is to specify with tag to execute with this runner
+		return Karate.run("classpath:features").tags("Regression");
+	}
+}
