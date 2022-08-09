348fb2f852970da766a33f7e7a99d96bc1c28da6
mrjstudent <reza.jamaly@tekschool.us>
2022-08-09 13:40:39 -0700

    third change

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -7,9 +7,9 @@ public class TestRunner {
 		// if this path is wrong you won't be able to run anything.
 		// tag is to specify with tag to execute with this runner
 		return Karate.run("classpath:features").tags("Regression");
+		System.out.println("third change");
 	}
-	private void sysout() {
-		sysout();
+	
 
 	}
 	
